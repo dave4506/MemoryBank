bc941aa6451655fc90daf0e77370c4abb1eb3c49
Stewart Vohra <stewartvhr@Stewarts-MacBook-Air.local>
2019-11-23 22:21:58 -0500

    My current, untested Xcode project for the app containing sign-in and sign-up code and event code. There is only the storyboard for the event functionality, no code for it, and the sign-in and sign-up is almost ready.--authorStewart Vohra <stewartvhr@gmail.com>

---
+++ frontend/memory_bank_app/memorybank/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  memorybank
+//
+//  Created by Stewart Vohra on 11/23/19.
+//  Copyright © 2019 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
