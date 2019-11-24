f2409a50753c74ac1f1092c2bce69b3d1d7973f3
Stewart Vohra <stewartvhr@Stewarts-MacBook-Air.local>
2019-11-24 00:40:22 -0500

    My current, untested Xcode project for the app containing sign-in and sign-up code and event code. Event code has been updated with untested code for create and delete event, and cuntested code for view events--authorStewart Vohra <stewartvhr@gmail.com>

---
+++ frontend/memory_bank_app/memorybank/CreateEventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateEventViewController.swift
+//  memorybank
+//
+//  Created by Stewart Vohra on 11/23/19.
+//  Copyright © 2019 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ frontend/memory_bank_app/memorybank/DeleteEventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteEventViewController.swift
+//  memorybank
+//
+//  Created by Stewart Vohra on 11/24/19.
+//  Copyright © 2019 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class DeleteEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
