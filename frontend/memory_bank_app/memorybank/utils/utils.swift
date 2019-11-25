31095f71c182250fe1c44f4a9eb2d21650f700d4
Benjamin Turner <bentur@umich.edu>
2019-11-25 14:01:55 -0500

    Correct UI format for identify

--- frontend/memory_bank_app/memorybank/utils/utils.swift
+++ frontend/memory_bank_app/memorybank/utils/utils.swift
@@ -17,3 +17,55 @@ func getDocumentsDirectory() -> URL {
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
 }
+
+func uploadImageToS3(imageData: Data) {
+   
+    var request = URLRequest(url:URL(string: "Https://memorybank-staging.herokuapp.com/upload/search")!)
+    request.httpMethod = "GET"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.addValue(getUserSession()?.token ?? "NULL", forHTTPHeaderField: "Authorization")
+    
+    let task = URLSession.shared.dataTask(with: request){ data, response, error in
+        guard let _ = data, error == nil else {
+            print("NETWORKING ERROR")
+            print("Networking error is: ", error)
+            return
+        }
+        if let httpStatus = response as? HTTPURLResponse,
+        httpStatus.statusCode != 200 {
+            print("HTTP STATUS: \(httpStatus.statusCode)")
+            return
+        }
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
+            DispatchQueue.main.async(execute: {
+                
+                upload(imageData: imageData, urlString: ((json["url"] as! String?)!), mimeType: "image"){ _,_ in print("Done") }
+            })
+        }
+        catch let error as NSError {
+                print("NSError is: ", error)
+        }
+
+        
+    }
+    task.resume()
+}
+
+func upload(imageData: Data, urlString: String, mimeType: String, completion: @escaping (Bool, Error?) -> Void) {
+    let requestURL = URL(string: urlString)!
+    var request = URLRequest(url: requestURL)
+    request.httpMethod = "PUT"
+    request.httpBody = imageData
+    request.setValue(mimeType, forHTTPHeaderField: "Content-Type")
+    request.setValue("\(imageData.count)", forHTTPHeaderField: "Content-Length")
+    request.setValue("public-read", forHTTPHeaderField: "x-amz-acl")
+    let task = URLSession.shared.dataTask(with: request, completionHandler: { (response, responseObject, error) in
+        print(response ?? "response nil")
+        print(error ?? "response nil")
+        completion(error == nil, error)
+    })
+    task.resume()
+}
--- frontend/memory_bank_app/memorybank/viewControllers/S3ImageUploadViewController.swift
+++ frontend/memory_bank_app/memorybank/viewControllers/S3ImageUploadViewController.swift
@@ -2,7 +2,7 @@ import Foundation
 import AWSCognitoIdentityProvider
 import AWSS3
 
-class S3ImageUploadViewController : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class S3ImageUploadViewController : UIViewController, UINavigationControllerDelegate {
     
     // Mark: Properties
     @IBOutlet weak var imageView: UIImageView!
@@ -20,92 +20,4 @@ class S3ImageUploadViewController : UIViewController, UIImagePickerControllerDel
         super.viewWillAppear(animated)
         self.navigationController?.setToolbarHidden(false, animated: true)
     }
-    
-    @IBAction func useCamera(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
-        pickerController.sourceType = .camera
-        pickerController.cameraCaptureMode = .photo
-        present(pickerController, animated: true)
-    }
-    
-    @IBAction func useImageLibrary(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        pickerController.delegate = self
-        pickerController.sourceType = .savedPhotosAlbum
-        present(pickerController, animated: true)
-    }
-    
-    // MARK: - UIImagePickerControllerDelegate
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-        imageView.image = selectedImage
-        // Dismiss the picker.
-        dismiss(animated: true, completion: nil)
-        let jpegData:Data = selectedImage.jpegData(compressionQuality: 0.95)!
-        
-        // var key: String
-        
-        // key = ""
-        
-        self.uploadImageToS3(imageData: jpegData)
-    }
-    
-    //MARK: - AWS Methods
-    func uploadImageToS3(imageData: Data) {
-       
-        var request = URLRequest(url:URL(string: "Https://memorybank-staging.herokuapp.com/upload/search")!)
-        request.httpMethod = "GET"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue(getUserSession()?.token ?? "NULL", forHTTPHeaderField: "Authorization")
-        
-        let task = URLSession.shared.dataTask(with: request){ data, response, error in
-            guard let _ = data, error == nil else {
-                print("NETWORKING ERROR")
-                print("Networking error is: ", error)
-                return
-            }
-            if let httpStatus = response as? HTTPURLResponse,
-            httpStatus.statusCode != 200 {
-                print("HTTP STATUS: \(httpStatus.statusCode)")
-                return
-            }
-            
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!) as! [String:Any]
-                DispatchQueue.main.async(execute: {
-                    
-                    self.upload(imageData: imageData, urlString: ((json["url"] as! String?)!), mimeType: "image"){ _,_ in print("Done") }
-                })
-            }
-            catch let error as NSError {
-                    print("NSError is: ", error)
-            }
-
-            
-        }
-        task.resume()
-    }
-    
-    func upload(imageData: Data, urlString: String, mimeType: String, completion: @escaping (Bool, Error?) -> Void) {
-        let requestURL = URL(string: urlString)!
-        var request = URLRequest(url: requestURL)
-        request.httpMethod = "PUT"
-        request.httpBody = imageData
-        request.setValue(mimeType, forHTTPHeaderField: "Content-Type")
-        request.setValue("\(imageData.count)", forHTTPHeaderField: "Content-Length")
-        request.setValue("public-read", forHTTPHeaderField: "x-amz-acl")
-        let task = URLSession.shared.dataTask(with: request, completionHandler: { (response, responseObject, error) in
-            print(response ?? "response nil")
-            print(error ?? "response nil")
-            completion(error == nil, error)
-        })
-        task.resume()
-    }
-    
 }
