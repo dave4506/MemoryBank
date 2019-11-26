fb51c5838a5d883fe696c65927d8d80fd79cf722
David Sun <ffstudios1@gmail.com>
2019-11-26 01:10:13 -0500

    refactored code

--- frontend/memory_bank_app/memorybank/viewControllers/CreateEventViewController.swift
+++ frontend/memory_bank_app/memorybank/viewControllers/CreateEventViewController.swift
@@ -7,115 +7,97 @@
 //
 
 import UIKit
+import Eureka
+import Alamofire
 
-class CreateEventViewController: UIViewController, UITextFieldDelegate {
+class CreateEventViewController: FormViewController {
+
+    var eventName: String?
+    var descript: String?
+    var location: String?
+    var startDate: Date?
+    var endDate: Date?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        //Set text field delegates to self
-        eventNameField.delegate = self
-        descripField.delegate = self
-        locationField.delegate = self
-        startTimeField.delegate = self
-        endTimeField.delegate = self
-        
-        //Initialize text fields to nil
-        self.eventNameField.text = nil
-        self.descripField.text = nil
-        self.locationField.text = nil
-        self.startTimeField.text = nil
-        self.endTimeField.text = nil
+        self.configureForm()
     }
     
-    //MARK: Attributes:
+    @IBAction func dismissClicked(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
     
-    var jwtToken: String?
+    func configureForm() {
+        form +++ Section()
+            <<< TextRow("eventName"){
+                // $0.hidden = "$segments != 'Family Member'"
+                $0.title = "Event Name"
+                $0.placeholder = "Family Reunion"
+            }.cellUpdate { cell, row in
+                self.eventName = cell.textField.text
+            }
+            <<< TextAreaRow("description"){
+                // $0.hidden = "$segments != 'Family Member'"
+                $0.title = "Description"
+                $0.placeholder = "What are we gonna do!"
+            }.cellUpdate { cell, row in
+                self.descript = cell.textView.text
+            }
+            <<< TextRow("location"){
+                // $0.hidden = "$segments != 'Family Member'"
+                $0.title = "Location"
+                $0.placeholder = "Where will it be?"
+            }.cellUpdate { cell, row in
+                self.location = cell.textField.text
+            }
+            <<< DateTimeRow() {
+                $0.title = "Start"
+            }.cellUpdate { cell, row in
+                self.startDate = cell.datePicker.date
+            }
+            <<< DateTimeRow() {
+                $0.title = "End"
+            }.cellUpdate { cell, row in
+                self.endDate = cell.datePicker.date
+            }
+            <<< ButtonRow(){
+                $0.title = "Create Event"
+            }.onCellSelection { cell, row in
+                self.saveEvent()
+            }
+    }
     
+    func handleEmpty() {
+        
+    }
     
-    @IBOutlet weak var eventNameField: UITextField!
-    
-    @IBOutlet weak var descripField: UITextField!
-    
-    @IBOutlet weak var locationField: UITextField!
-    
-    @IBOutlet weak var startTimeField: UITextField!
-    
-    @IBOutlet weak var endTimeField: UITextField!
-    
-    //MARK: Actions:
-    
-    @IBAction func saveEvent(_ sender: UIButton) {
-        guard let nameValue = self.eventNameField.text, !nameValue.isEmpty,
-        let descripValue = self.descripField.text, !descripValue.isEmpty,
-        let locationValue = self.locationField.text, !locationValue.isEmpty,
-        let startValue = self.startTimeField.text, !startValue.isEmpty,
-        let endValue = self.endTimeField.text, !endValue.isEmpty
-         else {
-            let alertController = UIAlertController(title: "Missing information",
-                                                    message: "Please enter valid event name, description, location, starting time, and ending time",
-                                                    preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-            alertController.addAction(okAction)
-            
-            self.present(alertController, animated: true, completion:  nil)
-            return
+    func saveEvent() {
+        guard let eventName = self.eventName, !eventName.isEmpty,
+        let descript = self.descript, !descript.isEmpty,
+        let location = self.location, !location.isEmpty,
+            let startDate = self.startDate, let endDate = self.endDate else { self.handleEmpty(); return }
+        
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        
+        let parameters = [
+            "event_name": eventName,
+            "event_description": descript,
+            "event_location": location,
+            "event_start_time": df.string(from: startDate),
+            "event_end_time": df.string(from: endDate),
+        ] as Parameters
+        
+        let headers = [
+            "Authorization": getUserSession()?.token ?? ""
+        ] as HTTPHeaders
+        
+        Alamofire.request("https://memorybank-staging.herokuapp.com/event/create", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .responseJSON { response in
+                if let _ = response.error {
+                } else {
+                    self.dismiss(animated: true)
+                }
         }
-        
-            let appDelegate = UIApplication.shared.delegate as! AppDelegate
-            self.jwtToken = appDelegate.getJWTToken()
-            
-            let json: [String: Any] =
-                ["event_name": self.eventNameField.text ?? "NULL",
-                 "event_description": self.descripField.text ?? "NULL",
-                "event_location": self.locationField.text ?? "NULL",
-                "event_start_time": self.startTimeField.text ?? "NULL",
-                "event_end_time": self.endTimeField.text ?? "NULL"]
-            
-            let jsonData = try? JSONSerialization.data(withJSONObject: json)
-            
-            var request = URLRequest(url:URL(string: "Https://memorybank-staging.herokuapp.com/event/create")!)
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue(self.jwtToken ?? "NULL", forHTTPHeaderField: "Authorization")
-            request.httpBody = jsonData
-            
-            let task = URLSession.shared.dataTask(with: request)
-                { data, response, error in
-                    guard let _ = data, error == nil else {
-                        print("NETWORKING ERROR")
-                        print("Networking error is: ", error)
-                        return
-                    }
-                    if let httpStatus = response as? HTTPURLResponse,
-                httpStatus.statusCode != 200 {
-                    print("HTTP STATUS: \(httpStatus.statusCode)")
-                    return
-                }
-
-                    DispatchQueue.main.async(execute: {
-                        let alertController = UIAlertController(title: "Event Successfully Created", message: "Event created. You can create another event or return to home page using the Back button. ",preferredStyle: .alert)
-                        
-                        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-                        alertController.addAction(okAction)
-                        
-                        self.present(alertController, animated: true, completion:  nil)
-                    })
-                }
-                task.resume()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ frontend/memory_bank_app/memorybank/views/EventTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EventTableViewCell.swift
+//  memorybank
+//
+//  Created by Stewart Vohra on 11/23/19.
+//  Copyright © 2019 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locLabel: UILabel!
+    
+    @IBOutlet weak var startLabel: UILabel!
+    
+    @IBOutlet weak var endLabel: UILabel!
+    
+}
